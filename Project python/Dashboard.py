e24f11c21032b68f340dc7f498d345e0aaf639c8
jejerarama <jeremy.rama@edu.devinci.fr>
2026-01-09 00:13:17 +0100

    Price and Data loading

--- Project python/Dashboard.py
+++ Project python/Dashboard.py
@@ -6,3 +6,25 @@ from plotly.subplots import make_subplots
 import time
 from datetime import datetime
 import yfinance as yf
+
+@st.cache_data(ttl=60)
+def get_live_price(symbol):
+    try:
+        live_data = yf.download(symbol, period="1d", interval="1m", auto_adjust=True, progress=False)
+        if not live_data.empty:
+            return float(live_data['Close'].values[-1])
+    except:
+        pass
+    return None
+
+
+def load_data(symbol, period):
+    data = yf.download(
+        symbol,
+        period=period,
+        interval="1d",
+        auto_adjust=True,
+        progress=False
+    )
+    data = data[['Open', 'High', 'Low', 'Close', 'Volume']].dropna()
+    return data
