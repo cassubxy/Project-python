c7f9c108faae9bf3737e0055380f057423f60e49
jejerarama <jeremy.rama@edu.devinci.fr>
2026-01-09 00:14:13 +0100

    Strategies Method

--- Project python/Dashboard.py
+++ Project python/Dashboard.py
@@ -28,3 +28,59 @@ def load_data(symbol, period):
     )
     data = data[['Open', 'High', 'Low', 'Close', 'Volume']].dropna()
     return data
+
+def buy_and_hold(data, capital):
+    data = data.copy()
+    data['Return'] = data['Close'].pct_change()
+    data['Cumulative_Return'] = (1 + data['Return']).cumprod()
+    
+    buy_price = float(data['Close'].iloc[0])
+    shares = capital / buy_price
+    final_value = float(shares * data['Close'].iloc[-1])
+    
+    return {
+        "portfolio_values": capital * data['Cumulative_Return'],
+        "final_value": final_value,
+        "profit": final_value - capital,
+        "profit_pct": float((final_value - capital) / capital * 100)
+    }
+
+
+def momentum_strategy(df, capital, short_window=20, long_window=50):
+    df = df.copy()
+    df = df.dropna()
+    df['MA_Short'] = df['Close'].rolling(short_window).mean()
+    df['MA_Long'] = df['Close'].rolling(long_window).mean()
+    
+    df['Signal'] = 0
+    df.loc[df['MA_Short'] > df['MA_Long'], 'Signal'] = 1
+    
+    cash = capital
+    shares = 0
+    position = 0
+    portfolio_values = []
+
+    for i in range(1, len(df)):
+        price = float(df['Close'].iloc[i])
+        signal = df['Signal'].iloc[i-1]
+
+        if signal == 1 and position == 0:
+            shares = cash / price
+            cash = 0
+            position = 1
+        elif signal == 0 and position == 1:
+            cash = shares * price
+            shares = 0
+            position = 0
+
+        portfolio_values.append(shares * price if position == 1 else cash)
+
+    results = pd.DataFrame({
+        'Date': df.index[1:],
+        'Portfolio_Value': portfolio_values
+    })
+    results['Strategy_Return'] = results['Portfolio_Value'].pct_change()
+    
+    return results, float(portfolio_values[-1])
+
+
